38b7d9a18750c9da1ccb601fc0cbe4591e5dfa89
valeros <valeros@bigmir.net>
2020-03-18 11:27:40 +0200

    Add initial support for Briki core // Resolve #97

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -34,4 +34,9 @@ build_mcu = env.get("BOARD_MCU", board.get("build.mcu", ""))
 MCU_FAMILY = board.get(
     "build.system", "sam" if build_mcu.startswith("at91") else "samd")
 
-env.SConscript(os.path.join("arduino", "arduino-%s.py" % MCU_FAMILY))
+if env.BoardConfig().get("build.core", "").lower() == "mbcwb":
+    env.SConscript(
+        os.path.join(env.PioPlatform().get_package_dir(
+            "framework-arduino-mbcwb"), "tools", "platformio-samd-build.py"))
+else:
+    env.SConscript(os.path.join("arduino", "arduino-%s.py" % MCU_FAMILY))
--- platform.py
+++ platform.py
@@ -35,6 +35,8 @@ class AtmelsamPlatform(PlatformBase):
             upload_tool = "tool-avrdude"
         elif upload_protocol == "jlink":
             upload_tool = "tool-jlink"
+        elif upload_protocol == "mbctool":
+            upload_tool = "tool-mbctool"
 
         if upload_tool:
             for name, opts in self.packages.items():
@@ -53,6 +55,9 @@ class AtmelsamPlatform(PlatformBase):
 
             if build_core != "arduino":
                 framework_package += "-" + build_core
+            if build_core == "mbcwb":
+                self.packages["tool-mbctool"]["optional"] = False
+                framework_package = "framework-arduino-mbcwb"
 
             self.frameworks["arduino"]["package"] = framework_package
             if not board.get("build.mcu", "").startswith("samd"):
