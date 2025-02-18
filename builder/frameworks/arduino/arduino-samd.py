e8762602dee858e091093795cf911049388b2748
Alex <33939600+4project-co-il@users.noreply.github.com>
2025-02-18 16:58:35 +0200

    Add support for custom boards variants directory (#238)

--- builder/frameworks/arduino/arduino-samd.py
+++ builder/frameworks/arduino/arduino-samd.py
@@ -24,6 +24,8 @@ http://arduino.cc/en/Reference/HomePage
 
 import os
 
+from arduino_common import get_variants_dir
+
 from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
@@ -41,7 +43,7 @@ CMSIS_ATMEL_DIR = platform.get_package_dir("framework-cmsis-atmel")
 
 assert all(os.path.isdir(d) for d in (FRAMEWORK_DIR, CMSIS_DIR, CMSIS_ATMEL_DIR))
 
-env.SConscript("arduino-common.py")
+env.SConscript("arduino_common.py")
 
 BUILD_CORE = "arduino"
 if VENDOR_CORE == "sparkfun" and board.get("build.mcu", "").startswith("samd51"):
@@ -178,9 +180,7 @@ elif VENDOR_CORE == "arduino":
 libs = []
 
 if "build.variant" in board:
-    variants_dir = os.path.join(
-        "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
-            "build.variants_dir", "") else os.path.join(FRAMEWORK_DIR, "variants")
+    variants_dir = get_variants_dir()
 
     env.Append(
         CPPPATH=[os.path.join(variants_dir, board.get("build.variant"))]
